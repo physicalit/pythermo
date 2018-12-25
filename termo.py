f6f90395fc0746644ebba3ed8acd738a498464aa
Mihuleac Sergiu <admin@physicalit.me>
2018-12-25 18:52:01 +0200

    bugfix

--- termo.py
+++ termo.py
@@ -27,8 +27,11 @@ def termoStat():
             for senz_temp in mycol.find():
                 senz_temp = senz_temp["temp"]
             mycol = db["senzor_out"]
-            for out_temp in mycol.find():
-                out_temp = out_temp["temp"]
+                for out_temp in mycol.find():
+                    try:
+                        out_temp = out_temp["temp"]
+                    except:
+                        pass
         except:
             time.sleep(10)
             continue
